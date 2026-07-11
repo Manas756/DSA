e5482db3cfe73645ba190f2b53869bad35749bed
Manas <manu4f4@gmail.com>
2026-07-12 01:39:20 +0530

    Merge sort

---
+++ Merge Sort Approach/main.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//0(n)
+
+void merge(vector<int> &arr,int st,int mid,int end){
+    vector<int> temp;
+
+    int i=st,j=mid+1;
+    while(i<=mid && j<=end){
+    if(arr[i]<=arr[j]){
+        temp.push_back(arr[i]);
+        i++;
+    }else{
+        temp.push_back(arr[j]);
+        j++;
+    }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+
+    }
+    while(j<=end){
+        temp.push_back(arr[j]);
+        j++;
+
+    }
+    for(int idx=0;idx<temp.size();idx++){
+        arr[idx+st]=temp[idx];
+    }
+
+}
+
+void mergeSort(vector<int> &arr,int st,int end){
+    if(st<end){
+        int mid=st+(end-st)/2;
+
+        mergeSort(arr,st,mid);//left
+        mergeSort(arr,mid+1,end);//right
+
+
+        merge(arr,st,mid,end);
+    }
+
+}
+
+
+
+
+int main(){
+    vector<int> arr={38,27,43,3,9,82,10};
+    mergeSort(arr,0,arr.size()-1);
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
