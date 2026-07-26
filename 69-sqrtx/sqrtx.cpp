630ca936b7b68ade6185ff6bc7527710210a55ec
Manas <157053068+Manas756@users.noreply.github.com>
2026-07-26 19:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.90%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1;
+        int r=x;
+        while(l<=r){
+            long long m=l+(r-l)/2;
+           long long msqr=m*m;
+            if(msqr==x)
+            return m;
+            else if(msqr<x)
+            l=m+1;
+            else r=m-1;
+            
+        }
+        return r;
+        
+    }
+};
