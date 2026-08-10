25388f950ba66720e93827df96f8a5e2f182fa2e
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-10 18:37:59 +0530

    Time: 31 ms (81.44%) | Memory: 8.7 MB (93.89%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,26 +1,15 @@
-static const int LIMIT=1e5;
-vector<bool> win(LIMIT+1,false);
-vector<int> square;
-auto init=[](){
-    for(int i=1;i<=sqrt(1e5);++i){
-        square.push_back(i*i);
-    }
-        for(int i=1;i<=LIMIT;++i){
-        for(int& s : square){
-            if(s>i) break;
-            if(!win[i-s]){
-                win[i]=true;
-                break;
-            }
-        }
-    }
-return 0;
-}();
 class Solution {
 public:
-
     bool winnerSquareGame(int n) {
+        vector<bool> win(n + 1, false);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                if(!win[i - j*j]) {
+                    win[i] = true;
+                    break;
+                }
+            }
+        }
         return win[n];
-        
     }
 };
