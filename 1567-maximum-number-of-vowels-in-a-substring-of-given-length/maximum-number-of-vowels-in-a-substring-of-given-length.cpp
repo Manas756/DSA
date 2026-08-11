3438dbbf8ae0260aae87ac92c613c26836ffcfb0
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-12 00:05:40 +0530

    Time: 4 ms (74.67%) | Memory: 13.1 MB (59.75%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -6,14 +6,16 @@ bool isVowel(char c) {
 }
 int maxVowels(string s, int k) {
     int count = 0;
+    // Count vowels in the first window of size k
     for (int i = 0; i < k; i++) {
         if (isVowel(s[i]))
             count++;
     }
     int maxi = count;
+   // Start sliding the window
 
     for (int i = k; i < s.length(); i++) {
-
+ //the character at i-k is no longer part of the window
         if (isVowel(s[i-k]))
             count--;
 
