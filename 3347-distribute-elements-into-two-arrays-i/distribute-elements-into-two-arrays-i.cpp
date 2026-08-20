8c0748cd4091a35ae7cee8f3e7dd8015f7ce01d0
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-20 19:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (66.37%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
    int n=nums.size();
-   vector<int> arr1={nums[0]};
-   vector<int> arr2 ={nums[1]};
+   vector<int> arr1;
+   vector<int> arr2;
+   arr1={nums[0]};
+   arr2={nums[1]};
    for(int i=2;i<n;i++){
       if(arr1[arr1.size()-1]>arr2[arr2.size()-1]){
          arr1.push_back(nums[i]);
@@ -13,7 +15,10 @@ public:
       }
 
    }
-   arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+   for(auto x:arr2){
+    arr1.push_back(x);
+
+   }
    return arr1;
   
     }
