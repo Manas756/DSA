da35fed483e88a70bc0c0c656beb3fb0e5785200
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-21 18:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (82.93%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.cpp
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -3,6 +3,9 @@ public:
       string reversePrefix(string s, int k) {
             int x=0;
             int y=k-1;
+            if(k==1){
+                return s;
+            }
             while(x<y){
                
                 swap(s[x],s[y]);
