794427618f531c7b9f670aec09dfca5acbc9b843
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-23 15:04:35 +0530

    Time: 7 ms (21.45%) | Memory: 14.1 MB (33.57%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n=num.length();
+        int leftKnownSum=0;
+        int rightKnownSum=0;
+
+        int leftQnMarkCount = 0;
+        int rightQnmarkCount =0;
+
+        for(int i=0;i<n;i++){
+            if(num[i]=='?'){
+                if(i<n/2){
+                    leftQnMarkCount++;
+
+                }
+                else{
+                    rightQnmarkCount++;
+
+                }
+                }else{
+                    if(i<n/2){
+                    leftKnownSum+=num[i]-'0';
+
+                }
+                else{
+                    rightKnownSum+=num[i]-'0';
+
+
+                }
+            }
+    
+        }
+        int totalQnMarks= leftQnMarkCount +rightQnmarkCount;
+        if(totalQnMarks%2==1){//alice will win
+            return true;
+        }
+        int left=2*leftKnownSum +9*leftQnMarkCount;
+        int right=2*rightKnownSum+9*rightQnmarkCount;
+
+        if(left==right)//bob win
+            return false;
+
+
+            return true;
+        
+    }
+};
