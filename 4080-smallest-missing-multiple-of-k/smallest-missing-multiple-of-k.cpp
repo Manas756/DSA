f8c22a9458753288fd3ec08423a4ac3bf7a5820b
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-25 11:10:42 +0530

    Time: 6 ms (12.42%) | Memory: 25.1 MB (45.06%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+    int n=nums.size();
+    if(k==1 && nums[n-1]==100){
+        return 101;
+
+    }
+    unordered_set<int> mp;
+    for(int x:nums){
+        mp.insert(x);
+    }
+   
+    for(int i=1;i<=100;i++){//so we need to find multiple of k elements
+    if(mp.find(k*i)==mp.end()){
+        return k*i;
+        
+    }
+    else{
+        continue;
+    }
+        
+    }
+    return -1;
+}
+};
