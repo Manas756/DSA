3c7be430fbbb7e5b8693a95ca965a1d8097fbb93
Manas <157053068+Manas756@users.noreply.github.com>
2026-08-28 17:06:30 +0530

    Time: 2 ms (76.92%) | Memory: 11.4 MB (84.62%) - LeetSync

---
+++ 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    string lexPalindromicPermutation(string s, string target) {
+        int n = s.size(), m = n / 2;
+        vector<int> freq(26);
+
+        for(char c : s)
+            freq[c-'a']++;
+
+        int odd = 0, mid = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] % 2) {
+                odd++;
+                mid = 'a' + i;
+            }
+        }
+
+        if(odd > 1) return "";
+
+        vector<int> cnt(26);
+
+        for(int i = 0; i < 26; i++)
+            cnt[i] = freq[i] / 2;
+
+        int z = 0;
+
+        while(z < m && cnt[target[z]-'a'] > 0) {
+            cnt[target[z]-'a']--;
+            z++;
+        }
+
+        if(z == m) {
+            string left = target.substr(0, m);
+            string right = left;
+
+            reverse(right.begin(), right.end());
+
+            string ans = left;
+            if(n % 2) ans += mid;
+            ans += right;
+
+            if(ans > target)
+                return ans;
+        }
+
+        for(int i = z; i >= 0; i--) {
+
+            if(i < z)
+                cnt[target[i]-'a']++;
+
+            int x = target[i]-'a';
+            int bigger = -1;
+
+            for(int c = x+1; c < 26; c++) {
+                if(cnt[c] > 0) {
+                    bigger = c;
+                    break;
+                }
+            }
+
+            if(bigger == -1)
+                continue;
+
+            string left = target.substr(0, i);
+            left += char('a' + bigger);
+
+            cnt[bigger]--;
+
+            for(int c = 0; c < 26; c++) {
+                while(cnt[c] > 0) {
+                    left += char('a' + c);
+                    cnt[c]--;
+                }
+            }
+
+            string right = left;
+            reverse(right.begin(), right.end());
+
+            if(n % 2)
+                return left + string(1, mid) + right;
+
+            return left + right;
+        }
+
+        return "";
+    }
+};
