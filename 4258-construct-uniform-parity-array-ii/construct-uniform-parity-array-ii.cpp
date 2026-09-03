a63599a396c996c586a5efa6f1a48051a01e8bee
Manas <157053068+Manas756@users.noreply.github.com>
2026-09-03 13:36:41 +0530

    Time: 4 ms (65.18%) | Memory: 173.7 MB (11.52%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minElement(vector<int> array){
+        int min = INT_MAX;
+        for(int i : array) if(i < min) min = i;
+        return min;
+    }
+    bool uniformArray(vector<int>& nums1) {
+        int evenCount{};
+        int minElem = minElement(nums1);
+        for(int i : nums1) if(i%2==0) evenCount++;
+        int oddCount = nums1.size() - evenCount;
+        if(evenCount < oddCount){
+            for(auto i : nums1) if(i%2 == 0){
+                if(!((i - minElem) % 2 == 1)) return false;
+            }
+            return true;
+        }
+        else{
+            for(auto i : nums1) if(i % 2 == 1){
+                if(!((i - minElem) % 2 == 0)) return false; 
+            }
+            return true;
+        }
+        return false;
+    }
+};
