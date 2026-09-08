aa3564767ceabda6135ee9a8f76154b0e9fac51f
Manas <157053068+Manas756@users.noreply.github.com>
2026-09-08 09:39:56 +0530

    Time: 3 ms (13.67%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int countCommas(int n) {
-            if(n<1000) return 0;
+         int count = 0;
 
-            
-            return (n-1000)+1;
+    for (int i = 1; i <= n; i++) {
+      if (i >= 1000) {
+        count++;
+      }
+    }
+
+    return count;
         
     }
 };
