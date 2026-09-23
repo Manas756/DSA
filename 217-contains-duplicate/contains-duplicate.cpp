692bb556ddecc8c767b2ab467da7c20ce4b7ff71
Manas <157053068+Manas756@users.noreply.github.com>
2026-09-24 01:25:30 +0530

    Time: 68 ms (66.70%) | Memory: 113.4 MB (20.42%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int k : nums ){
+            mp[k]++;
+        }
+
+        for(auto &i:mp){
+            if(i.second>1) return true;
+        }
+        return false;
+        
+    }
+};
